568dc8c216e23c3b2ad31860a7f8a9ba9cfde89f
Malator19 <43792652+Malator19@users.noreply.github.com>
2018-10-16 23:11:43 +0200

    Add files via upload

---
+++ GTK+/callback.h
@@ -0,0 +1,16 @@
+#ifndef CALLBACK_H
+#define CALLBACK_H
+
+#include <gtk/gtk.h>
+
+void cb_new (GtkWidget *, gpointer);
+void cb_open (GtkWidget *, gpointer);
+void cb_save (GtkWidget *, gpointer);
+void cb_saveas (GtkWidget *, gpointer);
+void cb_close (GtkWidget *, gpointer);
+void cb_quit (GtkWidget *, gpointer);
+void cb_modifie (GtkWidget *, gpointer);
+void cb_about (GtkWidget *, gpointer);
+
+
+#endif
