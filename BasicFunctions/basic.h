042a5cfc2c8a88bebfdb7c020156b60c03855ab2
louis Le Gatt <louis.le-gatt@epita.fr>
2018-10-20 20:57:03 +0200

    dvnk,

--- BasicFunctions/basic.h
+++ BasicFunctions/basic.h
@@ -3,3 +3,5 @@
 
 void putpixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel);
 Uint32 getpixel(SDL_Surface *surface, unsigned x, unsigned y);
+static inline Uint8* pixelref(SDL_Surface *surf, unsigned x, unsigned y);
+
