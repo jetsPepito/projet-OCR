ec9df9182a161a809def1190d4cbd030a0545b98
Antoine Gonzalez <SpeedyCraft51@gmail.com>
2018-10-20 22:45:06 +0200

    Fix absolute path
    Signed-off-by: Antoine Gonzalez <SpeedyCraft51@gmail.com>

--- Segmentation/Segmentation.c
+++ Segmentation/Segmentation.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL/SDL.h>
-#include "../SPE_Project/projet-OCR/BasicFunctions/basic.h"
+#include "../BasicFunctions/basic.h"
 #include "Segmentation.h"
 
 
